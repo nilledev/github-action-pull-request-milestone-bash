54b6c8e43e130b50afbaee21a2ea7e26a66e2cf5
Nille af Ekenstam <n@nille.dev>
2021-02-07 22:34:10 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,14 @@
 #!/usr/bin/env bash
 
-echo "This is my action and it's working!"
+if [[ "$GITHUB_EVENT_NAME" != "pull_request" ]]; then
+  echo "This action only runs on pull_request.closed"
+  echo "Found: $GITHUB_EVENT_NAME"
+  exit 1
+fi
+
+ACTION=$(jq -r ".action" $GITHUB_EVENT_PATH)
+if [[ "$ACTION" != " closed" ]]; then
+  echo "This action only runs on pull_request.closed"
+  echo "Found: $GITHUB_EVENT_NAME.$ACTION"
+  exit 1
+fi
